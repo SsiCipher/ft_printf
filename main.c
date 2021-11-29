0e0cebc6d3f7814ec8cc15b81b5971c14ab66dae
SsiCipher <ssicipher@mail.com>
2021-11-29 20:06:09 +0100

    Beautify :)

--- main.c
+++ main.c
@@ -4,8 +4,10 @@ int	main()
 {
 	int	n = 20;
 	int	*p = &n;
-	int	fttotal = ft_printf("%% My name is: %s(%c) I'm %d or %i or in hex %x/%X the pointer to it is %p the unsigned %u\n", "Cipher", 'M', n, n, n + 43, n + 43, p, 4294967295);
-	int	systotal = printf("%% My name is: %s(%c) I'm %d or %i or in hex %x/%X the pointer to it is %p the unsigned %lu\n", "Cipher", 'M', n, n, n + 43, n + 43, p, 4294967295);
+	int	fttotal = ft_printf("%%\nMy name is: %s(%c)\nI'm %d or %i\nor in hex %x/%X\nthe pointer to it is %p\nthe unsigned %u\n", "Cipher", 'M', n, n, n + 43, n + 43, p, 4294967295);
+	ft_printf("\n");
+	int	systotal = printf("%%\nMy name is: %s(%c)\nI'm %d or %i\nor in hex %x/%X\nthe pointer to it is %p\nthe unsigned %lu\n", "Cipher", 'M', n, n, n + 43, n + 43, p, 4294967295);
+	ft_printf("\n");
 	printf("Total sys printf: %d, total ft_printf: %d\n", systotal, fttotal);
 	return 0;
 }
